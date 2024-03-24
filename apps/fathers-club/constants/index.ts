3da329fdb8911ec2803782267a93d5ea6aae2c05
RishiDevGit <rishirajsaxena@makewebbetter.com>
2024-03-24 23:37:28 +0530

    constants and utilities file changes

---
+++ apps/fathers-club/constants/index.ts
@@ -0,0 +1,4 @@
+import * as DefaultValueConstants from './defaultValueConstants';
+import * as GeneralConstants from './generalConstants';
+import * as HeaderLinkConstants from './headerLinkConstants';
+export { DefaultValueConstants, GeneralConstants, HeaderLinkConstants };
